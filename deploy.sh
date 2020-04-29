d8f6293cd05e5c43567346e07462861c607d748e
omar.silva <omar.silva@globant.com>
2020-04-29 17:27:00 -0300

    'fixed travis'

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+docker build -t zefhon/multi-client -f ./client/Dockerfile ./client
+
+docker build -t zefhon/multi-worker -f ./worker/Dockerfile ./worker
+
+docker build -t zefhon/multi-server -f ./server/Dockerfile ./server
+
+docker push zefhon/multi-client
+docker push zefhon/multi-server
+docker push zefhon/multi-worker
